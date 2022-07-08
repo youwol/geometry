3bc80cd83e798b3ae2e3ec3f244732c3f8c4c3df
greinisch <greinisch@youwol.com>
2022-07-08 10:59:52 +0200

    🔧 webpack.config.js => add bundle-analyzer + auto format

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,54 @@
-const path = require('path');
-const webpack = require('webpack');
-const pkg = require('./package.json');
-const ROOT = path.resolve(__dirname, 'src');
-const DESTINATION = path.resolve(__dirname, 'dist');
+const path = require('path')
+const webpack = require('webpack')
+const pkg = require('./package.json')
+const ROOT = path.resolve(__dirname, 'src')
+const DESTINATION = path.resolve(__dirname, 'dist')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = {
     context: ROOT,
     entry: {
-        'main': './index.ts'
+        main: './index.ts',
     },
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            reportFilename: './bundle-analysis.html',
+            openAnalyzer: false,
+        }),
+    ],
     output: {
         path: DESTINATION,
         libraryTarget: 'umd',
         umdNamedDefine: true,
         library: pkg.name,
-        filename: pkg.name + ".js",
-        globalObject: `(typeof self !== 'undefined' ? self : this)`
+        filename: pkg.name + '.js',
+        globalObject: `(typeof self !== 'undefined' ? self : this)`,
     },
     resolve: {
         extensions: ['.ts', 'tsx', '.js'],
-        modules: [
-            ROOT,
-            'node_modules'
-        ]
+        modules: [ROOT, 'node_modules'],
     },
-    externals: [{
-        // 'rxjs': "rxjs",
-        // 'rxjs/operators': {
-        //     commonjs:'rxjs/operators',
-        //     commonjs2:'rxjs/operators',
-        //     root:['rxjs','operators']
-        // },
-        '@youwol/dataframe': "@youwol/dataframe",
-        '@youwol/math': "@youwol/math"
-    }],
+    externals: [
+        {
+            // 'rxjs': "rxjs",
+            // 'rxjs/operators': {
+            //     commonjs:'rxjs/operators',
+            //     commonjs2:'rxjs/operators',
+            //     root:['rxjs','operators']
+            // },
+            '@youwol/dataframe': '@youwol/dataframe',
+            '@youwol/math': '@youwol/math',
+        },
+    ],
     module: {
         rules: [
             {
                 test: /\.ts$/,
-                use: [
-                    { loader: 'ts-loader' },
-                  ],
-                  exclude: /node_modules/,
-            }
+                use: [{ loader: 'ts-loader' }],
+                exclude: /node_modules/,
+            },
         ],
     },
-    devtool: 'source-map'
-};
+    devtool: 'source-map',
+}
