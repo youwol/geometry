ca648c7dcb761baa30632753f2fc2cf057b92ac5
Xaliphostes <fmaerten@gmail.com>
2023-08-21 08:35:16 +0200

    before-arch3

--- examples/harmonic.js
+++ examples/harmonic.js
@@ -3,14 +3,14 @@ const geom = require('../../../../workspace/packages/geometry/dist/@youwol/geome
 const fs = require('fs')
 
 // let dataframe = io.decodeGocadTS( fs.readFileSync('/Users/fmaerten/data/mesh/S1.ts', 'utf8') )[0]
-let dataframe = geom.generateRectangle({ a: 20, b: 15, na: 30, nb: 30 })
+let dataframe = geom.generateRectangle({ a: 20, b: 15, na: 50, nb: 50 })
 
 const laplace = new geom.HarmonicDiffusion(
     dataframe.series.positions,
     dataframe.series.indices,
     0,
 )
-laplace.constrainsBorders(0)
+// laplace.constrainsBorders(0)
 laplace.addConstraint([0, 0, 0], 3)
 laplace.addConstraint([-5, -5, 0], -3)
 laplace.epsilon = 0.6
@@ -19,7 +19,11 @@ laplace.maxIter = 5000
 
 // dataframe.series['P'] = laplace.solve({record: true})
 
-dataframe = laplace.solve({ name: 'P', record: false, step: 10 })
+dataframe = laplace.solve({
+    name: 'P',
+    record: true,
+    step: 100
+})
 
 fs.writeFileSync(
     '/Users/fmaerten/data/mesh/laplace.ts',
